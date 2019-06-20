93cb151308a311a880a7efa21d4c36cf5631910e
John <johnba@nih.gov>
2019-06-20 18:53:38 -0400

    binary search implemented

---
+++ binarysearch.py
@@ -0,0 +1,29 @@
+
+
+
+def main():
+    arr = ['a', 'b', 'c', 'd', 'f', 'g','h','i']
+    target = 'd'
+    res = doSearch(arr, target)
+    print('done', res)
+
+def doSearch(arrIn, targetIn):
+    lo = 0
+    hi = len(arrIn)-1
+
+    while lo < hi:
+        mid = (hi + lo)//2
+        guess = arrIn[mid]
+        print(mid, guess)
+        if guess == targetIn:
+            return mid
+        elif guess < targetIn:
+            lo = mid + 1
+        else:
+            hi = mid - 1
+
+    return hi
+
+
+
+main()
