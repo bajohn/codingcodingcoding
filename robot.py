48559c84df91355785fe7684a9a5e9e349402ba5
Brendan John <bjohn454@gmail.com>
2019-08-19 20:29:44 -0400

    robot factorial

---
+++ robot.py
@@ -0,0 +1,8 @@
+import math 
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        return self.fact(m + n - 2)/(self.fact(m-1) * self.fact(n-1)
+        
+        
+    def fact(self, n):
+        return math.factorial(n)
